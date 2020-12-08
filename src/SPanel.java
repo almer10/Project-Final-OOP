f27c5f92af787e2e7902c11db62de048db6c807b
petani kode <hadyanalmer@gmail.com>
2020-12-09 00:15:19 +0700

    Update

--- src/SPanel.java
+++ src/SPanel.java
@@ -3,11 +3,6 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Panel;
 
-/**
- * 
- * Class ini untuk membuat panel yang digunakan untuk batas pada main window
- * 
- */
 public class SPanel extends Panel // create border panels for the display
 {
     private static final long serialVersionUID = 1L;
